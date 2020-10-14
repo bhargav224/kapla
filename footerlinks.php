ac5bc8b70917bc35fc3f83655ff66482d8ee670b
bhargav224 <myjava224@gmail.com>
2020-10-14 03:48:47 -0700

    intial commit

---
+++ footerlinks.php
@@ -0,0 +1,18 @@
+  <!-- base:js -->
+  <script src="vendors/base/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="js/template.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <script src="vendors/chart.js/Chart.min.js"></script>
+    <script src="vendors/progressbar.js/progressbar.min.js"></script>
+		<script src="vendors/chartjs-plugin-datalabels/chartjs-plugin-datalabels.js"></script>
+		<script src="vendors/justgage/raphael-2.1.4.min.js"></script>
+		<script src="vendors/justgage/justgage.js"></script>
+    <!-- Custom js for this page-->
+    <script src="js/dashboard.js"></script>
+    <!-- End custom js for this page-->
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include 'headerlinks.php';?>
+  </head>
+  <body>
+  <div class="container-scroller">
+	    <?php include 'header.php'; ?>
+		<div class="container-fluid page-body-wrapper">
+			<div class="main-panel">
+				
+			</div>
+			<!-- main-panel ends -->
+		</div>
+		<!-- page-body-wrapper ends -->
+    </div>
+        <?php include 'footerlinks.php';?>
+  </body>
+</html>
